0dc690e6409766a2e5069e13521f2bfe48e46148
Katie <katie.boydy@gmail.com>
2018-08-19 16:53:33 +0100

    updated with more complex rules: now overall score is maintained and the score for the round is taken away if the player rolls a 1

--- Javascript/Pig game/app.js
+++ Javascript/Pig game/app.js
@@ -1,42 +1,46 @@
 var scores, roundscore, activePlayer;
 
 scores = [0,0];
-roundScore = 0;
+roundScore = [0,0];
 activePlayer = 0;
 
 var diceImg = document.querySelector('.dice');
 
 diceImg.style.display = 'none';
-document.querySelector('#score-0'). textContent = '0';
-document.querySelector('#score-1'). textContent = '0';
+document.querySelector('#score-0').textContent = '0';
+document.querySelector('#score-1').textContent = '0';
 document.querySelector('#current-0').textContent = '0';
 document.querySelector('#current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	var dice = Math.floor(Math.random() * 6) + 1;
-	document.querySelector('#current-' + activePlayer).textContent = dice;
 	diceImg.style.display = "block";
 	diceImg.src = "dice-" + dice + ".png";
 	
-	if(dice !== 1){
+	if(dice != 1){
 		scores[activePlayer] = scores[activePlayer] + dice;
+		roundScore[activePlayer] = roundScore[activePlayer] + dice;
 	}
-	else{
-		scores[activePlayer] = 0;
+	else if (dice == 1){
+		scores[activePlayer] = scores[activePlayer] - roundScore[activePlayer];
+		
 	}
-
-	document.querySelector('#score-' + activePlayer). textContent = scores[activePlayer];
-
-	if(activePlayer === 0 && dice === 1){
-		activePlayer = 1;
-	}
-	else if(activePlayer === 1 && dice === 1){
-		activePlayer = 0;
-	}
+	
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+	document.querySelector('#current-' + activePlayer).textContent = roundScore[activePlayer];
+	 if (dice == 1){
+	 	reset();
+	 }
 
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-
+	reset();
 });
+
+function reset(){
+	roundScore[activePlayer] = 0;
+	document.querySelector('#current-' + activePlayer).textContent = roundScore[activePlayer];
+	activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+	count = 0;
+}
